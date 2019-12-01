2da20a009d505b67da991b25f61781ab668e1afa
WillemRB <wrb@outlook.com>
2019-12-01 15:23:05 +0100

    Improve solution for day 1

--- 2019/day01.swift
+++ 2019/day01.swift
@@ -1,16 +1,14 @@
 import Foundation
 
 func determineFuel(value: Int) -> Int {
-  let fuel = ((value / 3) - 2)
+  let fuel = value / 3 - 2
 
   return fuel > 0 ? fuel + determineFuel(value: fuel) : 0
 }
 
-let contents = (try String(contentsOfFile: "input.txt")).split(separator: "\n")
+let input = (try String(contentsOfFile: "input.txt")).split(separator: "\n").map { Int($0) }
 
-let input = contents.map { Int($0) }
-
-let fuelA = input.reduce(0) { $0 + (($1! / 3) - 2) }
+let fuelA = input.reduce(0) { $0 + ($1! / 3) - 2 }
 let fuelB = input.reduce(0) { $0 + determineFuel(value: $1!) }
 
 print("Part One: \(fuelA)")
