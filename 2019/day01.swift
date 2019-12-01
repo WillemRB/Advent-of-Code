bd03c8538de02d50f61a960928b8415ec3e7338e
WillemRB <wrb@outlook.com>
2019-12-01 15:07:37 +0100

    1B

--- 2019/day01.swift
+++ 2019/day01.swift
@@ -1,9 +1,17 @@
 import Foundation
 
+func determineFuel(value: Int) -> Int {
+  let fuel = ((value / 3) - 2)
+
+  return fuel > 0 ? fuel + determineFuel(value: fuel) : 0
+}
+
 let contents = (try String(contentsOfFile: "input.txt")).split(separator: "\n")
 
 let input = contents.map { Int($0) }
 
-let result = input.reduce(0) { $0 + (($1! / 3) - 2) }
+let fuelA = input.reduce(0) { $0 + (($1! / 3) - 2) }
+let fuelB = input.reduce(0) { $0 + determineFuel(value: $1!) }
 
-print(result)
+print("Part One: \(fuelA)")
+print("Part Two: \(fuelB)")
