9ba1f16b29c122cf8c8b1d4ed126a616d76ff3bd
Willem <WillemRB@users.noreply.github.com>
2019-12-06 09:59:22 +0100

    2019 - 5B

--- 2019/day05.swift
+++ 2019/day05.swift
@@ -1,34 +1,45 @@
 import Foundation
 
-func processOps(input: Int) throws -> Int {
+func processOps(input: Int) throws {
   var intOp = try String(contentsOfFile: "input.txt").split(separator: ",").map{ Int($0)! }
 
-  var index = 0
-  while (intOp[index] != 99) {
-    if(intOp[index] % 100 == 1) {
-      let p1 = ((intOp[index] / 100) % 10) == 0 ? intOp[intOp[index + 1]] : intOp[index + 1]
-      let p2 = ((intOp[index] / 1000) % 10) == 0 ? intOp[intOp[index + 2]] : intOp[index + 2]
-      intOp[intOp[index + 3]] = p1 + p2
-      index += 4
+  var pointer = 0
+  while (intOp[pointer] != 99) {
+    let p1 = ((intOp[pointer] / 100) % 10) == 0 ? intOp[intOp[pointer + 1]] : intOp[pointer + 1]
+    
+    var p2 = 0
+    if (intOp[pointer] % 100 != 3 && intOp[pointer] % 100 != 4) {
+      p2 = (intOp[pointer] / 1000) == 0 ? intOp[intOp[pointer + 2]] : intOp[pointer + 2]
     }
-    else if (intOp[index] % 100 == 2) {
-      let p1 = ((intOp[index] / 100) % 10) == 0 ? intOp[intOp[index + 1]] : intOp[index + 1]
-      let p2 = ((intOp[index] / 1000) % 10) == 0 ? intOp[intOp[index + 2]] : intOp[index + 2]
-      intOp[intOp[index + 3]] = p1 * p2
-      index += 4
-    }
-    else if (intOp[index] % 100 == 3) {
-      intOp[intOp[index + 1]] = input
-      index += 2
-    }
-    else if (intOp[index] % 100 == 4) {
-      let p1 = (intOp[index] / 100) == 0 ? intOp[intOp[index + 1]] : intOp[index + 1]
-      print("Output[\(index)]: \(p1)")
-      index += 2
+
+    switch (intOp[pointer] % 100) {
+      case 1:
+        intOp[intOp[pointer + 3]] = p1 + p2
+        pointer += 4
+      case 2:
+        intOp[intOp[pointer + 3]] = p1 * p2
+        pointer += 4
+      case 3:
+        intOp[intOp[pointer + 1]] = input
+        pointer += 2
+      case 4:
+        print("Output[\(pointer)]: \(p1)")
+        pointer += 2
+      case 5:
+        pointer = p1 != 0 ? p2 : pointer + 3
+      case 6:
+        pointer = p1 == 0 ? p2 : pointer + 3
+      case 7:
+        intOp[intOp[pointer + 3]] = p1 < p2 ? 1 : 0
+        pointer += 4
+      case 8:
+        intOp[intOp[pointer + 3]] = p1 == p2 ? 1 : 0
+        pointer += 4
+      default:
+        break
     }
   }
-
-  return intOp[0]
 }
 
-let _ = try processOps(input: 1)
+try processOps(input: 1) // A
+try processOps(input: 5) // B
