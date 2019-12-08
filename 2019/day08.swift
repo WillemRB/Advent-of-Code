dfe6fa36b6f5fa416b470db5c429df8ec9c03818
Willem <WillemRB@users.noreply.github.com>
2019-12-08 13:42:20 +0100

    2019 - 8B

--- 2019/day08.swift
+++ 2019/day08.swift
@@ -9,19 +9,41 @@ let imageSize = width * height
 var zeroes = imageSize
 var result = 0
 
+var message = ""
+
 while (!image.isEmpty) {
   let layer = image.prefix(imageSize)
   image = String(image.suffix(image.count - imageSize))
 
-  var hashmap: [Character: Int] = [:]
-  String(layer).forEach {
-    hashmap[$0] = (hashmap[$0] ?? 0)! + 1
-  }
+  // A
+  // var hashmap: [Character: Int] = [:]
+  // String(layer).forEach {
+  //   hashmap[$0] = (hashmap[$0] ?? 0)! + 1
+  // }
+  // print(layer)
 
-  if (hashmap["0"] ?? 0 < zeroes) {
-    zeroes = hashmap["0"]!
-    result = (hashmap["1"]!) * (hashmap["2"]!)
+  // if (hashmap["0"] ?? 0 < zeroes) {
+  //   print(hashmap["0"]!)
+  //   zeroes = hashmap["0"]!
+  //   result = (hashmap["1"]!) * (hashmap["2"]!)
+  // }
+
+  if (message.isEmpty) {
+    message = String(layer)
+  }
+  else {
+    var decoded = ""
+    for c in layer {
+      let char = message.removeFirst()
+      decoded.append(char == "2" ? c : char)
+    }
+    message = decoded
   }
 }
 
-print("Result: \(result)")
+//print("Result: \(message)") // A
+
+while (!message.isEmpty) {
+  print(message.prefix(width).map { $0 == "0" ? "\u{20}" : "\u{2588}" }.joined(separator: ""))
+  message = String(message.suffix(message.count - width))
+}
