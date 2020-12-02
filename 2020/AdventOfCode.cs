f3c758ac014430825011bdff62dffca3faac7cab
WillemRB <willem.bouma@achmea.nl>
2020-12-02 10:15:52 +0100

    2020 - 2A & 2B

--- 2020/AdventOfCode.cs
+++ 2020/AdventOfCode.cs
@@ -4,7 +4,7 @@
     {
         static void Main()
         {
-            Day01();
+            Day02();
         }
     }
 }
---
+++ 2020/Day02.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode
+{
+    partial class AdventOfCode
+    {
+        static void Day02()
+        {
+            var regex = new Regex(@"^(?<min>\d+)-(?<max>\d+) (?<char>.): (?<password>.+)$");
+
+            var validPasswords = File.ReadAllLines("input/day02.txt")
+                .Where(line =>
+                {
+                    var match = regex.Match(line);
+                    var min = int.Parse(match.Groups["min"].Value);
+                    var max = int.Parse(match.Groups["max"].Value);
+                    var c = match.Groups["char"].Value.First();
+                    var password = match.Groups["password"].Value;
+
+                    // Part A
+                    //var count = password.Count(p => p == c);
+                    //return count >= min && count <= max;
+
+                    // Part B
+                    return password[min - 1] == c ^ password[max - 1] == c;
+                }).Count();
+
+            Console.WriteLine($"Valid passwords: {validPasswords}");
+        }
+    }
+}
