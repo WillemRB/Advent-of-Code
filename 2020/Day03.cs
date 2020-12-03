c46d2197de4385465858f639dc6512e9dce83405
WillemRB <willem.bouma@achmea.nl>
2020-12-03 09:26:20 +0100

    2020 - 3B

--- 2020/Day03.cs
+++ 2020/Day03.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.IO;
-using System.Linq;
 
 namespace AdventOfCode
 {
@@ -8,16 +7,27 @@ namespace AdventOfCode
     {
         static void Day03()
         {
-            var area = File.ReadAllLines("../../../input/day03.txt").ToList();
+            var area = File.ReadAllLines("../../../input/day03.txt");
 
-            int x = 0, trees = 0;
-            area.ForEach(l =>
-            {
-                trees += l[x % l.Length] == '#' ? 1 : 0;
-                x += 3;
-            });
+            // Part A
+            //Console.WriteLine($"Trees encountered: {TreesEncountered(area, 3)}");
 
+            // Part B
+            var trees = TreesEncountered(area, 1) * TreesEncountered(area, 3) * TreesEncountered(area, 5) * TreesEncountered(area, 7) * TreesEncountered(area, 1, 2);
             Console.WriteLine($"Trees encountered: {trees}");
+        }
+
+        static long TreesEncountered(string[] area, int slopeX, int slopeY = 1)
+        {
+            int x = 0, y = 0, trees = 0;
+            while (y < area.Length)
+            {
+                trees += area[y][x % area[y].Length] == '#' ? 1 : 0;
+                x += slopeX;
+                y += slopeY;
+            }
+
+            return trees;
         }
     }
 }
