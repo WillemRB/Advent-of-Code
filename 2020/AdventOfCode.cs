88e61713e14e73a66d17fcf88bfa6f1a86f743fe
WillemRB <willem.bouma@achmea.nl>
2020-12-03 08:59:12 +0100

    2020 - 3A

--- 2020/AdventOfCode.cs
+++ 2020/AdventOfCode.cs
@@ -4,7 +4,7 @@
     {
         static void Main()
         {
-            Day02();
+            Day03();
         }
     }
 }
---
+++ 2020/Day03.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    partial class AdventOfCode
+    {
+        static void Day03()
+        {
+            var area = File.ReadAllLines("../../../input/day03.txt").ToList();
+
+            int x = 0, trees = 0;
+            area.ForEach(l =>
+            {
+                trees += l[x % l.Length] == '#' ? 1 : 0;
+                x += 3;
+            });
+
+            Console.WriteLine($"Trees encountered: {trees}");
+        }
+    }
+}
