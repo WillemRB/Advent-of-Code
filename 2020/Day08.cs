b5fb12e18c5cebf930200b187bbf12fe808d7744
WillemRB <willem.bouma@achmea.nl>
2020-12-08 11:00:38 +0100

    2020 - 8B

--- 2020/Day08.cs
+++ 2020/Day08.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -6,18 +7,47 @@ namespace AdventOfCode
 {
     partial class AdventOfCode
     {
+        static List<Instruction> instructions;
+
         static void Day08()
         {
-            var instructions = File.ReadAllLines("../../../input/day08.txt")
+            instructions = File.ReadAllLines("../../../input/day08.txt")
                 .Select(r => new Instruction(r))
                 .ToList();
 
+            // Part A
+            //Console.WriteLine($"Value before a loop starts: {RunInstructions().Item1}");
+
+            // Part B
+            for (int i = 0; i < instructions.Count; i++)
+            {
+                instructions.ForEach(op => op.Processed = false);
+
+                if (instructions[i].Operation == "acc")
+                    continue;
+
+                var op = instructions[i].Operation;
+                instructions[i].Operation = op == "jmp" ? "nop" : "jmp";
+
+                var result = RunInstructions();
+                if (result.Item2)
+                {
+                    Console.WriteLine($"Value when program terminates: {result.Item1}");
+                    break;
+                }
+
+                instructions[i].Operation = op;
+            }
+        }
+
+        static Tuple<int, bool> RunInstructions()
+        {
             int head = 0, value = 0;
             while (!instructions[head].Processed)
             {
                 instructions[head].Processed = true;
 
-                switch(instructions[head].Operation)
+                switch (instructions[head].Operation)
                 {
                     case "acc":
                         value += instructions[head].Argument;
@@ -30,10 +60,12 @@ namespace AdventOfCode
                         head++;
                         break;
                 }
+
+                if (head == instructions.Count)
+                    return Tuple.Create(value, true);
             }
 
-            // Part A
-            Console.WriteLine($"Value before a loop starts: {value}");
+            return Tuple.Create(value, false);
         }
     }
 
