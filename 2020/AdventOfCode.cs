3cbf85c6e942a42d855a5d2ee074c8000f91847a
WillemRB <willem.bouma@achmea.nl>
2020-12-09 07:28:44 +0100

    2020 - 9A

--- 2020/AdventOfCode.cs
+++ 2020/AdventOfCode.cs
@@ -4,7 +4,7 @@
     {
         static void Main()
         {
-            Day08();
+            Day09();
         }
     }
 }
---
+++ 2020/Day09.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    partial class AdventOfCode
+    {
+        static readonly int preambleSize = 25;
+
+        static void Day09()
+        {
+            var xmas = File.ReadAllLines("../../../input/day09.txt")
+                .Select(long.Parse)
+                .ToList();
+
+            // Part A
+            for (int i = preambleSize; i < xmas.Count; i++)
+            {
+                var preamble = xmas.Skip(i - preambleSize).Take(preambleSize);
+
+                if (!preamble.Any(l => preamble.Contains(xmas[i] - l)))
+                    Console.WriteLine($"Invalid value: {xmas[i]}");
+            }
+        }
+    }
+}
