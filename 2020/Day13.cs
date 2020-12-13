cb58679719ee913562cb8101b68c70a7c1a14265
WillemRB <wrb@outlook.com>
2020-12-13 08:02:52 +0100

    Update 13A

--- 2020/Day13.cs
+++ 2020/Day13.cs
@@ -9,20 +9,15 @@ namespace AdventOfCode
         static void Day13()
         {
             var lines = File.ReadAllLines("../../../input/day13.txt");
-            
-            var earliest = int.Parse(lines[0]);
-            var buses = lines[1].Split(',')
-                .Where(b => b != "x").Select(b => (Bus: int.Parse(b), Departure: int.Parse(b) - (earliest % int.Parse(b))))
-                .ToList();
-
-            var firstBus = (Bus: 0, Departure: int.MaxValue);
-            for (int i = 0; i < buses.Count(); i++)
-            {
-                if (buses[i].Departure < firstBus.Departure)
-                    firstBus = buses[i];
-            }
 
             // Part A
+            var earliest = int.Parse(lines[0]);
+            var buses = lines[1].Split(',')
+                .Where(b => b != "x")
+                .Select(b => (Bus: int.Parse(b), Departure: int.Parse(b) - (earliest % int.Parse(b))))
+                .ToList();
+
+            var firstBus = buses.OrderBy(b => b.Departure).First();
             Console.WriteLine($"First bus: {firstBus.Bus * firstBus.Departure}");
         }
     }
