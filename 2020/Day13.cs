08eb9589ff59d75d763ee652848ed1cf3f006bdc
WillemRB <wrb@outlook.com>
2020-12-13 10:53:02 +0100

    2020 - 13B

--- 2020/Day13.cs
+++ 2020/Day13.cs
@@ -1,6 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 
 namespace AdventOfCode
 {
@@ -9,16 +9,34 @@ namespace AdventOfCode
         static void Day13()
         {
             var lines = File.ReadAllLines("../../../input/day13.txt");
+            var data = lines[1].Split(',');
 
-            // Part A
             var earliest = int.Parse(lines[0]);
-            var buses = lines[1].Split(',')
-                .Where(b => b != "x")
-                .Select(b => (Bus: int.Parse(b), Departure: int.Parse(b) - (earliest % int.Parse(b))))
-                .ToList();
+            var buses = new List<(int Index, int Bus, int Departure)>();
 
-            var firstBus = buses.OrderBy(b => b.Departure).First();
-            Console.WriteLine($"First bus: {firstBus.Bus * firstBus.Departure}");
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (data[i] == "x")
+                    continue;
+                buses.Add((i, int.Parse(data[i]), int.Parse(data[i]) - (earliest % int.Parse(data[i]))));
+            }
+
+            //Part A
+            //var firstBus = buses.OrderBy(b => b.Departure).First();
+            //Console.WriteLine($"First bus: {firstBus.Bus * firstBus.Departure}");
+
+            // Part B
+            var timestamp = 1L;
+            var step = 1L;
+
+            buses.ForEach(b =>
+            {
+                while ((timestamp + b.Index) % b.Bus != 0)
+                    timestamp += step;
+                step *= b.Bus;
+            });
+
+            Console.WriteLine($"Timestamp: {timestamp}");
         }
     }
 }
