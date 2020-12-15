a60cbac74e6919859baccfc312b6c3781b0c2b8e
WillemRB <willem.bouma@achmea.nl>
2020-12-15 07:22:40 +0100

    2020 - 15A

--- 2020/AdventOfCode.cs
+++ 2020/AdventOfCode.cs
@@ -4,7 +4,7 @@
     {
         static void Main()
         {
-            Day13();
+            Day15();
         }
     }
 }
---
+++ 2020/Day15.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    partial class AdventOfCode
+    {
+        static void Day15()
+        {
+            var turns = 2020;
+            var game = "8,13,1,0,18,9".Split(',').Select(int.Parse).ToList();
+
+            while (game.Count < turns)
+            {
+                var timesSpoken = game.Count(c => c == game.Last());
+
+                if (timesSpoken == 1)
+                    game.Add(0);
+                else
+                    game.Add(game.Count - (game.Take(game.Count - 1).ToList().LastIndexOf(game.Last()) + 1));
+            }
+
+            Console.WriteLine($"Answer at turn {turns}: {game.Last()}");
+        }
+    }
+}
