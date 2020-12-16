58d91981463a1961154e3577f2ac24a8c427ad75
WillemRB <willem.bouma@achmea.nl>
2020-12-16 11:56:56 +0100

    2020 - 15B

--- 2020/Day15.cs
+++ 2020/Day15.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace AdventOfCode
@@ -7,20 +8,45 @@ namespace AdventOfCode
     {
         static void Day15()
         {
-            var turns = 2020;
-            var game = "8,13,1,0,18,9".Split(',').Select(int.Parse).ToList();
+            // Part A
+            var resultA = PlayMemoryGame(2020, "8,13,1,0,18,9");
+            Console.WriteLine($"Answer at turn {resultA.turn}: {resultA.value}");
 
-            while (game.Count < turns)
+            // Part A
+            var resultB = PlayMemoryGame(30_000_000, "8,13,1,0,18,9");
+            Console.WriteLine($"Answer at turn {resultB.turn}: {resultB.value}");
+        }
+
+        static (int turn, int value) PlayMemoryGame(int turns, string start)
+        {
+            var game = start.Split(',').Select(int.Parse).ToList();
+            var memory = new Dictionary<int, List<int>>();
+            int value = default(int);
+
+            for (int turn = 1; turn <= turns; turn++)
             {
-                var timesSpoken = game.Count(c => c == game.Last());
-
-                if (timesSpoken == 1)
-                    game.Add(0);
+                if ((turn - 1) < game.Count)
+                {
+                    value = game[turn - 1];
+                    
+                    memory.Add(value, new List<int>());
+                    memory[value].Insert(0, turn);
+                }
                 else
-                    game.Add(game.Count - (game.Take(game.Count - 1).ToList().LastIndexOf(game.Last()) + 1));
+                {
+                    value = memory[value].Count == 1 ? 0 : memory[value][0] - memory[value][1];
+
+                    if (!memory.ContainsKey(value))
+                        memory.Add(value, new List<int>());
+
+                    memory[value].Insert(0, turn);
+
+                    if (memory[value].Count > 2)
+                        memory[value].RemoveAt(2);
+                }
             }
 
-            Console.WriteLine($"Answer at turn {turns}: {game.Last()}");
+            return (turns, value);
         }
     }
 }
