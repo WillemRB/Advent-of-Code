d0759826ec695cd52f38881542607e97d563fd5e
WillemRB <willem.bouma@achmea.nl>
2020-12-16 10:22:32 +0100

    2020 - 16B

--- 2020/Day16.cs
+++ 2020/Day16.cs
@@ -16,44 +16,67 @@ namespace AdventOfCode
                 .TakeWhile(l => !string.IsNullOrEmpty(l))
                 .Select(rule => new TicketRule(rule)).ToList();
 
-            var myTicket = new Ticket(lines
+            var myTicket = lines
                 .Skip(ticketRules.Count() + 2)
-                .First());
+                .First().Split(',').Select(int.Parse).ToArray();
 
             var tickets = lines
                 .Skip(ticketRules.Count() + 5)
-                .Select(t => new Ticket(t))
+                .Select(t => t.Split(',').Select(int.Parse).ToArray())
                 .ToList();
 
-            var errorRate = 0;
-            foreach (var ticket in tickets)
-            {
-                for(int i = 0; i < ticket.Fields.Count(); i++)
-                {
-                    if (ticketRules.Any(rule => rule.IsValid(ticket.Fields[i])))
-                        continue;
+            ticketRules.ForEach(t => t.InitializeValidPosition(myTicket.Length));
 
-                    errorRate += ticket.Fields[i];
+            var errorRate = 0;
+            foreach (var ticketFields in tickets)
+            {
+                var isValid = true;
+                for (int i = 0; i < ticketFields.Count(); i++)
+                {
+                    isValid &= ticketRules.Any(rule => rule.IsValid(ticketFields[i]));
+                    errorRate += !isValid ? ticketFields[i] : 0;
+                    if (!isValid)
+                        break;
+                }
+
+                if (!isValid)
+                    continue;
+
+                for (int i = 0; i < ticketFields.Count(); i++)
+                {
+                    ticketRules.ForEach(rule =>
+                    {
+                        if (!rule.IsValid(ticketFields[i]))
+                            rule.ValidPositions.Remove(i);
+                    });
                 }
             }
 
+            // Part A
             Console.WriteLine($"Ticket scanning error rate: {errorRate}");
-        }
 
-        class Ticket
-        {
-            public int[] Fields { get; set; }
-
-            public Ticket(string fields)
+            // Part B
+            var result = 1L;
+            var validPositions = new List<int>();
+            for (int i = 1; i < ticketRules.Count; i++)
             {
-                Fields = fields.Split(',').Select(int.Parse).ToArray();
+                var rule = ticketRules.Single(r => r.ValidPositions.Count == i);
+
+                var position = rule.ValidPositions.Except(validPositions).Single();
+                if (rule.Name.StartsWith("departure"))
+                    result *= myTicket[position];
+
+                validPositions = rule.ValidPositions;
             }
+            
+            Console.WriteLine($"Departure fields: {result}");
         }
 
         class TicketRule
         {
             public string Name { get; private set; }
             public List<int> Range { get; } = new List<int>();
+            public List<int> ValidPositions { get; private set; }
 
             public TicketRule(string rule)
             {
@@ -66,6 +89,8 @@ namespace AdventOfCode
                     Range.AddRange(Enumerable.Range(start, (end - start) + 1));
                 }
             }
+
+            public void InitializeValidPosition(int l) => ValidPositions = Enumerable.Range(0, l).ToList();
 
             public bool IsValid(int value) => Range.Contains(value);
         }
