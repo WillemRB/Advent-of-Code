653ee3cda523555126a7e4c73706ff8e47cebb66
WillemRB <wrb@outlook.com>
2022-12-04 11:45:44 +0100

    2022 - 3A,3B,4A,4B

---
+++ 2022/Day03.cs
@@ -0,0 +1,68 @@
+﻿namespace AdventOfCode
+{
+    public class Day03
+    {
+        private List<Rucksack> _rucksacks;
+
+        [SetUp]
+        public void Setup()
+        {
+            var lines = File.ReadAllLines(@"day03.txt");
+
+            _rucksacks = lines.Select(l => new Rucksack(l)).ToList();
+        }
+
+        [Test]
+        public void PartA()
+        {
+            var result = _rucksacks.Sum(r =>
+            {
+                var c = r.CommonComponent();
+                return char.IsLower(c) ? c - 96 : c - 38;
+            });
+
+            Assert.That(result, Is.EqualTo(157));
+        }
+
+        [Test]
+        public void PartB()
+        {
+            var result = 0;
+
+            for (var i = 0; i < _rucksacks.Count; i += 3)
+            {
+                var rucksack1 = _rucksacks.Skip(i).First();
+                var rucksack2 = _rucksacks.Skip(i + 1).First();
+                var rucksack3 = _rucksacks.Skip(i + 2).First();
+
+                var intersect = rucksack1.Contents.Intersect(rucksack2.Contents);
+                var common = intersect.Intersect(rucksack3.Contents).Single();
+
+                result += char.IsLower(common) ? common - 96 : common - 38;
+            }
+            
+            Assert.That(result, Is.EqualTo(70));
+        }
+    }
+
+    internal class Rucksack
+    {
+        public char[] Contents { get; private set; }
+
+        public char[] FirstCompartment { get; private set; }
+
+        public char[] SecondCompartment { get; private set; }
+
+        public Rucksack(string contents)
+        {
+            Contents = contents.ToCharArray();
+            FirstCompartment = contents.Substring(0, contents.Length / 2).ToCharArray();
+            SecondCompartment = contents.Substring(contents.Length / 2).ToCharArray();
+        }
+
+        public char CommonComponent()
+        {
+            return FirstCompartment.Intersect(SecondCompartment).Single();
+        }
+    }
+}
---
+++ 2022/Day04.cs
@@ -0,0 +1,69 @@
+﻿using System.Numerics;
+
+namespace AdventOfCode
+{
+    public class Day04
+    {
+        private List<SectionAssignment> _assignments;
+
+        [SetUp]
+        public void Setup()
+        {
+            var lines = File.ReadAllLines(@"day04.txt");
+
+            _assignments = lines.Select(line => new SectionAssignment(line)).ToList();
+        }
+
+        [Test]
+        public void PartA()
+        {
+            var result = _assignments.Count(a => a.IsFullyContained());
+
+            Assert.That(result, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void PartB()
+        {
+            var result = _assignments.Count(a => a.HasOverlap());
+
+            Assert.That(result, Is.EqualTo(4));
+        }
+    }
+
+    internal class SectionAssignment
+    {
+        private BigInteger _firstAssignment = new();
+        private BigInteger _secondAssignment = new();
+
+        public SectionAssignment(string pair)
+        {
+            var assignments = pair.Split(',').Select(Parse).ToArray();
+
+            _firstAssignment = assignments[0];
+            _secondAssignment = assignments[1];
+        }
+
+        public bool IsFullyContained()
+        {
+            var xor = _firstAssignment ^ _secondAssignment;
+            return (xor & _firstAssignment) == 0 || (xor & _secondAssignment) == 0;
+        }
+
+        public bool HasOverlap()
+        {
+            return (_firstAssignment & _secondAssignment) > 0;
+        }
+
+        private BigInteger Parse(string input)
+        {
+            BigInteger assignment = new();
+            var sections = input.Split('-').Select(int.Parse).ToArray();
+
+            for (var i = sections[0] - 1; i < sections[1]; i++)
+                assignment |= (BigInteger)Math.Pow(2, i);
+
+            return assignment;
+        }
+    }
+}
