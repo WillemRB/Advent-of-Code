f68f68b63309ca217cfdd919d62568ccd5da3c86
Willem Bouma <willem.bouma@achmea.nl>
2022-12-06 09:59:28 +0100

    2022 - 6A, 6B

---
+++ 2022/Day06.cs
@@ -0,0 +1,43 @@
+﻿namespace AdventOfCode
+{
+    public class Day06
+    {
+        private string _buffer;
+
+        [SetUp]
+        public void Setup()
+        {
+            _buffer = File.ReadAllText(@"day06.txt");
+        }
+
+        [Test]
+        public void PartA()
+        {
+            var marker = GetMarker(size: 4);
+
+            Assert.That(marker, Is.EqualTo(7));
+        }
+
+        [Test]
+        public void PartB()
+        {
+            var marker = GetMarker(size: 14);
+
+            Assert.That(marker, Is.EqualTo(19));
+        }
+
+        private int GetMarker(int size)
+        {
+            for (int i = 0; i <= _buffer.Length - size; i++)
+            {
+                var set = _buffer.Substring(i, size).ToHashSet();
+                if (set.Count == size)
+                {
+                    return i + size;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
